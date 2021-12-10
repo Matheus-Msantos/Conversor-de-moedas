6d43132e987f478beab2ece32ba4660b9218676f
Matheus-Msantos <matheusmelosantos10@gmail.com>
2021-12-10 19:41:04 -0300

    Criando mensagem de error e modal

--- conversor-de-moedas/src/Conversor-moedas.js
+++ conversor-de-moedas/src/Conversor-moedas.js
@@ -4,44 +4,65 @@ import { Button, Form, Col, Spinner } from 'react-bootstrap';
 
 function ConversorMoedas() {
   return (
-    <div className="c-moedas-container">
-      <h1>Conversor-moedas</h1>
-      <div className="c-moedas-container-convertor">
+    <>
+      <div className="c-moedas-container">
+        <h1>Conversor-moedas</h1>
 
-        <div className="c-moedas-box">
-          <input
-            className="c-moedas-box-campo"
-            type="number"
-            value={1}
-            required
-          />
+        <div className="c-moedas-error">
+          <span>Error</span>
         </div>
 
-        <div className="c-moedas-box">
-          <select className="c-moedas-box-campo">
+        <div className="c-moedas-container-convertor">
 
-          </select>
-        </div>
+          <div className="c-moedas-box">
+            <input
+              className="c-moedas-box-campo"
+              type="number"
+              required
+            />
+          </div>
 
-        <div className="c-moedas-box--icon">
-          <i className="fas fa-angle-double-right"></i>
-        </div>
+          <div className="c-moedas-box">
+            <select className="c-moedas-box-campo">
 
-        <div className="c-moedas-box">
-          <select className="c-moedas-box-campo">
+            </select>
+          </div>
 
-          </select>
-        </div>
+          <div className="c-moedas-box--icon">
+            <i className="fas fa-angle-double-right"></i>
+          </div>
 
-        <div className="c-moedas-box">
-          <button className="c-moedas-botao" type="submit">
-            <Spinner animation="border" size="sm"></Spinner>
-            Converter
-          </button>
+          <div className="c-moedas-box">
+            <select className="c-moedas-box-campo">
+
+            </select>
+          </div>
+
+          <div className="c-moedas-box">
+            <button className="c-moedas-botao" type="submit">
+              <Spinner animation="border" size="sm"></Spinner>
+              Converter
+            </button>
+          </div>
         </div>
       </div>
-    </div>
+      <div className="c-moedas-modal-overley">
+        <div className="c-moedas-modal-container">
+          <div className="c-moedas-modal-header">
+            <h3>Conversão</h3>
+            <i className="fas fa-times"></i>
+          </div>
 
+          <div className="c-moedas-modal-content">
+            <span>lorem ipsum....</span>
+          </div>
+
+          <div className="c-moedas-modal-footer">
+            <button>Nova conversão</button>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
